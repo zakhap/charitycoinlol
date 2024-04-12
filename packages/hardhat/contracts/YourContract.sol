78efed75c09ce969d4db3af3667cd9d7b0f29cb3
zakhap <zhs2104@columbia.edu>
2024-04-12 19:44:18 -0400

    Contracts and deployer

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,87 +1,179 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+
 
 // Useful for debugging. Remove when deploying to a live network.
 import "hardhat/console.sol";
+ 
+contract CharityCoin is ERC20, ReentrancyGuard {
+    constructor(
+        uint256 totalSupply,
+        string memory name,
+        string memory symbol,
+        address[] memory holders,
+        uint8[] memory percentages
+    ) ERC20(name, symbol) {
+        require(holders.length == percentages.length, "Mismatched holders and percentages arrays");
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
+        uint256 totalPercentages;
+        for (uint256 i = 0; i < holders.length; i++) {
+						totalPercentages += percentages[i];
+            _mint(holders[i], totalSupply * percentages[i] / 100);
+        }
+				require(totalPercentages == 100, "Percentages do not add up to 100");
 
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
-contract YourContract {
-	// State Variables
-	address public immutable owner;
-	string public greeting = "Building Unstoppable Apps!!!";
-	bool public premium = false;
-	uint256 public totalCounter = 0;
-	mapping(address => uint) public userGreetingCounter;
+    }
+}
 
-	// Events: a way to emit log statements from smart contract that can be listened to by external parties
-	event GreetingChange(
-		address indexed greetingSetter,
-		string newGreeting,
-		bool premium,
-		uint256 value
-	);
+interface IWETH {
+	function deposit() external payable;
+	function balanceOf(address account) external view returns (uint256);
+}
 
-	// Constructor: Called once on contract deployment
-	// Check packages/hardhat/deploy/00_deploy_your_contract.ts
-	constructor(address _owner) {
-		owner = _owner;
+interface IUniV3 {
+	function createPool(
+		address tokenA,
+		address tokenB,
+		uint24 fee
+	) external returns (address pool);
+}
+
+contract LiquidityProvider is ReentrancyGuard, Ownable {
+	IERC20Metadata public token;
+	uint256 public charityFee; // Fee in percentage
+	address public charityAddress;
+	bool public isActive;
+	uint256 public threshold;
+	address public uniswapV3;
+	address public weth;
+
+	constructor(uint256 _charityFee, address _charityAddress, address _uniswapV3, address _weth) {
+			charityFee = _charityFee;
+			charityAddress = _charityAddress;
+			isActive = false;
+			uniswapv3 = _uniswapV3;
+			weth = _weth;
 	}
 
-	// Modifier: used to define a set of rules that must be met before or after a function is executed
-	// Check the withdraw() function
-	modifier isOwner() {
-		// msg.sender: predefined variable that represents address of the account that called the current function
-		require(msg.sender == owner, "Not the Owner");
-		_;
+	function registerToken(address _token) external onlyOwner {
+		uint256 pooltotal = IERC20Metadata(_token).balanceOf(address(this));
+		require(pooltotal > 0);
+		threshold = pooltotal * 70 / 100;
+		token = IERC20Metadata(_token);
+		isActive = true;
+		transferOwnership(address(0));
 	}
 
 	/**
-	 * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-	 *
-	 * @param _newGreeting (string memory) - new greeting to save on the contract
+	 * @dev This function calculates the price of a token based on the current supply, the amount to be purchased, 
+	 * the number of decimals the token uses, and a scalar value. The calculation is done using a bonding curve formula.
+	 * 
+	 * @param supply The current total supply of the token.
+	 * @param amount The amount of tokens to be purchased.
+	 * @param decimals The number of decimals the token uses.
+	 * @param scalar A scalar value used to adjust the price calculation.
+	 * 
+	 * @return The calculated price for the specified amount of tokens.
 	 */
-	function setGreeting(string memory _newGreeting) public payable {
-		// Print data to the hardhat chain console. Remove when deploying to a live network.
-		console.log(
-			"Setting new greeting '%s' from %s",
-			_newGreeting,
-			msg.sender
-		);
+	function calculatePrice(uint256 supply, uint256 amount, uint256 decimals, uint256 scalar) public pure returns (uint256) {
+		// Calculate the first part of the summation formula. If the supply is 0, sum1 is 0.
+		uint256 sum1 = supply == 0 ? 0 : (supply - 1) * supply * (2 * (supply - 1) + 1) / 6;
+		
+		// Calculate the second part of the summation formula
+		uint256 sum2 = (supply == 0 && amount == 1) ? 0 : (supply - 1 + amount) * (supply + amount) * (2 * (supply - 1 + amount) + 1) / 6;
+		
+		// Calculate the difference between sum2 and sum1.
+		uint256 summation = sum2 - sum1;
+		return summation * (10 ** decimals) / scalar;
+	}
 
-		// Change state variables
-		greeting = _newGreeting;
-		totalCounter += 1;
-		userGreetingCounter[msg.sender] += 1;
+	function buy() external payable nonReentrant {
+		require(isActive == true);
+		uint256 supply = token.balanceOf(address(this));
+		uint256 fee = msg.value * charityFee / 100;
+		uint256 amount = calculatePrice(supply, msg.value - fee, token.decimals(), 1 ether);
+		// Transfer the tokens
+		token.transfer(msg.sender, amount);
+		payable(charityAddress).transfer(fee);
+		// on threshold hook to create unipool (V2/V3???)
+		if (supply < threshold) {
+			// call hook
+			// // 1. get weth
+			uint256 contractBalance = address(this).balance;
+			IWETH(weth).deposit{value: contractBalance}();
+			// 2. create pool
 
-		// msg.value: built-in global variable that represents the amount of ether sent with the transaction
-		if (msg.value > 0) {
-			premium = true;
-		} else {
-			premium = false;
+			// 3. Fund pool
 		}
-
-		// emit: keyword used to trigger an event
-		emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, msg.value);
 	}
 
-	/**
-	 * Function that allows the owner to withdraw all the Ether in the contract
-	 * The function can only be called by the owner of the contract as defined by the isOwner modifier
-	 */
-	function withdraw() public isOwner {
-		(bool success, ) = owner.call{ value: address(this).balance }("");
-		require(success, "Failed to send Ether");
-	}
+    function sell(uint256 amount) external nonReentrant {
+			require(isActive == true);
+			uint256 supply = token.balanceOf(address(this));
+			uint256 price = calculatePrice(supply - amount, amount, token.decimals(), 1 ether);
+			uint256 charityAmount = price * charityFee / 100;
+			uint256 totalReturn = price - charityAmount;
 
-	/**
-	 * Function that allows the contract to receive ETH
-	 */
-	receive() external payable {}
+			// Transfer the tokens
+			token.transferFrom(msg.sender, address(this), amount);
+
+			// Send the Ether
+			payable(charityAddress).transfer(charityAmount);
+			payable(msg.sender).transfer(totalReturn);
+    }
+}
+
+
+contract CharityCoinDeployer is ReentrancyGuard {
+    // Hardcoded address to receive 10% of the total supply
+    address public charityAddress;
+		address public uniswapV3;
+		address public weth; 
+
+		event NewTokenCreated(address indexed tokenAddress, string name, string symbol, uint256 totalSupply);
+
+    constructor(address _charityAddress, _uniswapV3, _weth) {
+			charityAddress = _charityAddress;
+			uniswapV3 = _uniswapV3;
+			weth = _weth;
+    }
+
+		function createCharityToken(
+        uint256 totalSupply,
+        string memory name,
+        string memory symbol
+		) external nonReentrant {
+
+			LiquidityProvider lpBaby = new LiquidityProvider(
+				3,
+				charityAddress,
+				uniswapV3,
+				weth
+			);
+
+			uint8[] memory percentages = new uint8[](2);
+			percentages[0] = 95;
+    	percentages[1] = 5;
+
+			address[] memory holders = new address[](2);
+			holders[0] = address(lpBaby);
+			holders[1] = charityAddress;
+			
+			CharityCoin token = new CharityCoin(
+				totalSupply,
+				name,
+				symbol,
+				holders,
+				percentages // 95% of the funds to the bonding curve, nonprofit gets to baghold for free
+			);
+
+			emit NewTokenCreated(address(token), name, symbol, totalSupply);
+
+		}
 }
